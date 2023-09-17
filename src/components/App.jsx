99ad8f90e06f8cc7b60c2aacf59d07c4b7e98162
nicksolony <nicksolony@gmail.com>
2023-09-17 18:32:15 -0400

    added number functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,32 +9,37 @@ export class App extends Component {
 
   state = {
     contacts: [],
-    name: ''
+    name: '',
+    number:''
   };
 
-  onNameInput = (e) => {
-      this.setState({name:e.target.value})
+  handleDataInput = (e) => {
+    const { name, value } = e.target;
+      this.setState({[name]:e.target.value})
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
     let contact = {
       id: nanoid(),
-      name: this.state.name
+      name: this.state.name,
+      number: this.state.number
     };
 
     this.setState(({ contacts }) => ({
       contacts: [...contacts, contact],
     }));
 
-    this.setState({ name: '' });
+    this.setState({
+      name: '',
+      number: ''});
   }
   
   
 
   render() {
 
-    let contacts = this.state.contacts;
+    let {contacts,name,number} = this.state;
 
     return (
       <div style={{
@@ -50,7 +55,6 @@ export class App extends Component {
         <h2>Phonebook</h2>
         <form onSubmit={this.handleSubmit}>
           <label htmlFor='name'>Name</label>
-
           <input
             type="text"
             name="name"
@@ -58,9 +62,21 @@ export class App extends Component {
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             id='name'
-            value={this.state.name}
-            onChange={this.onNameInput}
-            />  
+            value={name}
+            onChange={this.handleDataInput}
+          />
+          <label htmlFor='number'>Phone number</label>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            id='number'
+            value={number}
+            onChange={this.handleDataInput}
+          />
+          
           <button type='submit'>Add contact</button>
         </form>
 
@@ -69,7 +85,7 @@ export class App extends Component {
         <ul>
           {contacts.map((contact) =>
             {return (
-            <li key={contact.id}>{contact.name}</li>
+            <li key={contact.id}>{contact.name} {contact.number}</li>
             )}
           )}
         </ul>
