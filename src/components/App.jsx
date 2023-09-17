bff633f3f83fad6eef094aa1c1e96321542ad10c
nicksolony <nicksolony@gmail.com>
2023-09-17 17:42:46 -0400

    added form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
 
 
 
@@ -22,14 +24,23 @@ export class App extends Component {
         color: '#010101'
       }}
       >
-        <label>Name</label>      
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+
+        <form>
+          <label htmlFor='name'>Name</label>
+
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            id='name'
+            />
+            
+          <button type='submit'>Add contact</button>
+        </form>
+        
+        
       </div>
     );
   };
