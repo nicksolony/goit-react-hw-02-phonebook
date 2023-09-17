40b4b3742620b764d2e57a3e592e28fcce9114e8
nicksolony <nicksolony@gmail.com>
2023-09-17 17:37:45 -0400

    change app to class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
+import React, { Component } from 'react';
+
+
+
+
+export class App extends Component {
+
+  state = {
+    contacts: [],
+    name: ''
+  };
+
+  render() {
+
+    return (
+      <div style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
@@ -9,8 +21,16 @@ export const App = () => {
         fontSize: 40,
         color: '#010101'
       }}
-    >
-      React homework template
-    </div>
-  );
+      >
+        <label>Name</label>      
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </div>
+    );
+  };
 };
