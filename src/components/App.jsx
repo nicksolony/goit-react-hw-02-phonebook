8eb86b6720ba3b07483c567b018d492dc9e9b3cc
nicksolony <nicksolony@gmail.com>
2023-09-17 19:23:20 -0400

    moved out form functionality into separate element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
 
 
 
@@ -14,9 +15,7 @@ export class App extends Component {
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
   ],
-  filter: '',
-  name: '',
-  number: ''
+  filter: ''
 }
 
   handleDataInput = (e) => {
@@ -24,36 +23,28 @@ export class App extends Component {
       this.setState({[name]:e.target.value})
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
+  addNewContact = ({ name, number }) => {
+    
     let contact = {
-      id: nanoid(),
-      name: this.state.name,
-      number: this.state.number
+          id: nanoid(),
+          name: name,
+          number: number
     };
-
+    
     this.setState(({ contacts }) => ({
-      contacts: [...contacts, contact],
-    }));
-
-    this.setState({
-      name: '',
-      number: ''
-    });
-  };
-
+          contacts: [...contacts, contact],
+        }));
+  }
 
   filterContacts = () => {
     let { filter, contacts } = this.state;
     let normalizedFilter = filter.toLowerCase();
-
     return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
   
   
   
-
   render() {
 
     let {name,number,filter} = this.state;
@@ -72,32 +63,7 @@ export class App extends Component {
       }}
       >
         <h2>Phonebook</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor='name'>Name</label>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            id='name'
-            value={name}
-            onChange={this.handleDataInput}
-          />
-          <label htmlFor='number'>Phone number</label>
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            id='number'
-            value={number}
-            onChange={this.handleDataInput}
-          />
-          
-          <button type='submit'>Add contact</button>
-        </form>
+        <ContactForm onSubmit={this.addNewContact} />
 
         <h2>Contacts</h2>
         <label htmlFor='filter'>Find contacts by name</label>
